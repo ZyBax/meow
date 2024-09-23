47649b3efeff39db7eb0a82cc0c193e52754f0b0
ZyBax <144496373+ZyBax@users.noreply.github.com>
2024-09-22 18:47:14 -0600

    Added EIAA site.
    ALL CREDIT FOR THE DOG IMAGE GOES TO KIM NACES FROM THE NOUN PROJECT

---
+++ sw.js
@@ -0,0 +1,13 @@
+// sw.js
+self.addEventListener('install', function(event) {
+    console.log('Service worker installed.');
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
