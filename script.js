f8fc8e8757db6758c92be77b7944354a77e4360a
ZyBax <144496373+ZyBax@users.noreply.github.com>
2024-10-06 20:28:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-<!-- Google tag (gtag.js) -->
-    
+<script type="text/javascript" data-cmp-ab="1" src="https://cdn.consentmanager.net/delivery/autoblocking/e7168f9ce18a3.js" data-cmp-host="c.delivery.consentmanager.net" data-cmp-cdn="cdn.consentmanager.net" data-cmp-codesrc="16"></script>
+
+    <!-- Google tag (gtag.js) -->
 async src="https://www.googletagmanager.com/gtag/js?id=G-3MLX5P0FLD"
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
