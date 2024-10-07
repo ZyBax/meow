933e8320786764258b2d8b455642feabf7a91a6a
ZyBax <144496373+ZyBax@users.noreply.github.com>
2024-10-06 18:34:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const loggingUrl = 'https://script.google.com/macros/s/AKfycbyuMRoy_7_Nzukp9U-UxGk8cgh7RbKUyQTYlhbchV4Zw0Yyn4UpEZ9XBL2i2ReNw1_sAg/exec';
+const loggingUrl = 'https://meowsers.meowautos.workers.dev';
 
 window.onload = function() {
-    const userAgent = navigator.userAgent;
-    const referrer = document.referrer;
-    const pageUrl = window.location.href;
-
-    fetch(`${loggingUrl}?ip=${encodeURIComponent('Your_IP_Here')}&pageUrl=${encodeURIComponent(pageUrl)}&userAgent=${encodeURIComponent(userAgent)}&referrer=${encodeURIComponent(referrer)}`)
+    fetch(loggingUrl)
         .then(response => {
             if (!response.ok) {
                 console.error('Error logging visit:', response.statusText);
