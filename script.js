7e280c742228b86145f7f7ab52fc5c39cd4db6d9
ZyBax <144496373+ZyBax@users.noreply.github.com>
2024-10-06 18:00:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const loggingUrl = 'https://script.google.com/macros/s/AKfycbwkbu75xtpO0p_n2xBJIcjIEl2ykLHFGZebTetGH7MuEatFBnZRKO7IT83ddc39AB9NBg/exec';
+const loggingUrl = 'https://script.google.com/macros/s/AKfycbyuMRoy_7_Nzukp9U-UxGk8cgh7RbKUyQTYlhbchV4Zw0Yyn4UpEZ9XBL2i2ReNw1_sAg/exec';
 
 window.onload = function() {
-    fetch(loggingUrl)
+    const userAgent = navigator.userAgent;
+    const referrer = document.referrer;
+    const pageUrl = window.location.href;
+
+    fetch(`${loggingUrl}?ip=${encodeURIComponent('Your_IP_Here')}&pageUrl=${encodeURIComponent(pageUrl)}&userAgent=${encodeURIComponent(userAgent)}&referrer=${encodeURIComponent(referrer)}`)
         .then(response => {
             if (!response.ok) {
                 console.error('Error logging visit:', response.statusText);
