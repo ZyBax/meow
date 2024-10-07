6992f2aa86aafef384d30f1064383ee43e2fbbf5
ZyBax <144496373+ZyBax@users.noreply.github.com>
2024-10-06 20:39:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 <script type="text/javascript" data-cmp-ab="1" src="https://cdn.consentmanager.net/delivery/autoblocking/e7168f9ce18a3.js" data-cmp-host="c.delivery.consentmanager.net" data-cmp-cdn="cdn.consentmanager.net" data-cmp-codesrc="16"></script>
 
-    <!-- Google tag (gtag.js) -->
-async src="https://www.googletagmanager.com/gtag/js?id=G-3MLX5P0FLD"
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3MLX5P0FLD"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-3MLX5P0FLD');
+</script>
