4a3b3cf76b657620336cb93c8e792585fa9636fc
ZyBax <144496373+ZyBax@users.noreply.github.com>
2025-01-19 17:50:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-var meta = document.createElement('meta');
-meta.name = "viewport";
-meta.content = "width=device-width, initial-scale=1";
-document.getElementsByTagName('head')[0].appendChild(meta);
-
-var script = document.createElement('script');
-script.src = "https://example.com/your-script.js";
-document.getElementsByTagName('head')[0].appendChild(script);
-(function() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.setAttribute('data-cmp-ab', '1');
-    script.src = 'https://cdn.consentmanager.net/delivery/autoblocking/e7168f9ce18a3.js';
-    script.setAttribute('data-cmp-host', 'c.delivery.consentmanager.net');
-    script.setAttribute('data-cmp-cdn', 'cdn.consentmanager.net');
-    script.setAttribute('data-cmp-codesrc', '16');
-    document.head.appendChild(script);
-})();
-
 (function() {
     var gtagScript = document.createElement('script');
     gtagScript.async = true;
